50bafcb9a5a72ec7f817287c2f60cc01b9a4f198
suinua <maatinosippo@gmail.com>
2020-05-28 22:29:54 +0900

    fix

--- src/team_system/models/Game.php
+++ src/team_system/models/Game.php
@@ -10,23 +10,14 @@ class Game
      * @var GameId
      */
     private $id;
-    /**
-     * @var TeamId
-     */
-    private $redTeamId;
-    /**
-     * @var TeamId
-     */
-    private $blueTeamId;
 
-    public function __construct(GameId $id, TeamId $redTeamId, TeamId $blueTeamId) {
+
+    public function __construct(GameId $id) {
         $this->id = $id;
-        $this->redTeamId = $redTeamId;
-        $this->blueTeamId = $blueTeamId;
     }
 
     static function asNew(): Game {
-        return new Game(GameId::asNew(), TeamId::asNew(), TeamId::asNew());
+        return new Game(GameId::asNew());
     }
 
     /**
@@ -34,19 +25,5 @@ class Game
      */
     public function getId(): GameId {
         return $this->id;
-    }
-
-    /**
-     * @return TeamId
-     */
-    public function getRedTeamId(): TeamId {
-        return $this->redTeamId;
-    }
-
-    /**
-     * @return TeamId
-     */
-    public function getBlueTeamId(): TeamId {
-        return $this->blueTeamId;
     }
 }
